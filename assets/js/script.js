a838190e819d6876e950ea40981c2d6f5a0a740d
Garrett Kegel <ghkegel@gmail.com>
2021-07-25 10:22:17 -0700

    Added functionality to the save button. The save button now stores a key, the time of the task, and a value, the description within the task text field.

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,25 @@ function displayTime() {
 // Load the full document first, then run jQuery code
 $(function() {
     setInterval(displayTime, 1000);
+
+    // This uses the on function from jQuery to make the saveButton
+    // execute the anonymous function on the click of the button
+    $('#saveButton').on('click', function() {
+        // taskTime is assigned the value of the id that is
+        // the parent of the saveButton, which will be the id
+        // of the container, ie id="hourXX" where XX will be
+        // the corresponding hour
+        var taskTime = $(this).parent().attr('id');
+        // taskDescription is assigned the value of the saveButton's 
+        // siblings, namely the element with the description class.
+        var taskDescription = $(this).siblings('.description').val();
+        // The taskTime is stored to localstorage as the key
+        // and the taskDescription is stored to localStorage as
+        // value. setTime(key, value)
+        localStorage.setItem(taskTime, taskDescription);
+    });
+
+
 });
 
 
